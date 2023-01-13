ae58fe705f372d89617ba5adfd8f12132c2391a4
kv <kvoloshenko@gmail.com>
2023-01-13 16:57:48 +0300

    Refactoring

--- sql2uml_001.py
+++ sql2uml_001.py
@@ -1,9 +1,11 @@
 import pprint
 file_path='ocs-rerate-orchestration_tables_00.cql'
 i=0
+# loading ddl from the file
 with open(file_path, "r") as df:
     lines = [line for line in df]
 
+# parsing ddl
 tables = []
 all_objects = []
 cur_table = dict()
@@ -46,8 +48,9 @@ for l in lines:
             # print(f'primary_key_item={primary_key_item}')
             cur_table['primary_keys'].append(primary_key_item)
 
-    print(f'len(items)={len(items)} tableStart={tableStart} {items}')
+    # print(f'len(items)={len(items)} tableStart={tableStart} {items}')
 
+# Outputing data for PlantUML
 uml_start = """
 @startuml
 !define table(x) class x << (T,#FFAAAA) >>
@@ -83,7 +86,7 @@ with open(uml_file_path, 'w', encoding='utf8') as f:
     f.write(uml_start)
 
     for o in all_objects:
-        print(type(o), o)
+        # print(type(o), o)
         table_line = 'table(' + o['table_name'] + ') {\n'
         f.write(table_line)
         # here is columns info output
@@ -92,7 +95,7 @@ with open(uml_file_path, 'w', encoding='utf8') as f:
         for c in table_columns:
             # primary keys
             column_name = c['name']
-            print(f'column_name={column_name} {primary_keys}')
+            # print(f'column_name={column_name} {primary_keys}')
             if column_name in primary_keys:
                 column_line = 'primary_key(' + column_name + ') : ' + c['type'] + '\n'
             else:
@@ -102,6 +105,67 @@ with open(uml_file_path, 'w', encoding='utf8') as f:
         f.write('}\n\n')
 
     f.write(uml_end)
+
+# Outputing data for html
+html_start = """<html>
+<head>
+<title>ReRate Cassandra tables</title>
+</head>
+<body>
+<h1>ReRate Cassandra tables</h1>
+  
+<p>Here we define structures of data stored in DB and in Cache - for each table and cache structure column we provide name, type, description. Structures should be groupped by storage type (like Hazelcast, Oracle DB, Cassandra DB)</p>
+  
+
+"""
+# Outputing data for html
+html_end = """
+</body>
+</html>
+"""
+html_table_start = """
+<table>
+  <tr>
+    <th>Column Name</th>
+    <th>Type</th>
+    <th>Description</th>
+  </tr>
+"""
+html_table_end = """
+</table>
+"""
+
+html_file_path = 'uml_file.html'
+with open(html_file_path, 'w', encoding='utf8') as f:
+    f.write(html_start)
+    for o in all_objects:
+        # print(type(o), o)
+        table_name_l ='<h2>' + o['table_name'] + '</h2>\n'
+        f.write(table_name_l)
+        f.write(html_table_start)
+
+        table_columns = o['columns']
+        primary_keys = o['primary_keys']
+        for c in table_columns:
+            f.write('<tr>\n')
+
+            # primary keys
+            column_name = c['name']
+            column_type = c['type']
+            column_type = column_type.replace(">", "&#62;")
+            column_type = column_type.replace("<", "&#60;")
+            # print(f'column_name={column_name} {primary_keys}')
+            if column_name in primary_keys:
+                column_line = '<td>' + column_name + '</td><td>' + column_type + '</td><td>primary_key</td>\n'
+            else:
+                column_line = '<td>' + column_name + '</td><td>' + column_type + '</td><td></td>\n'
+            f.write(column_line)
+            f.write('</tr>\n')
+
+        f.write(html_table_end)
+
+
+    f.write(html_end)
 
 # print(lines)
 # pprint.pprint(lines)
